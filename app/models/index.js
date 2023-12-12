b111b09dd6c38b2d8c7ae899c9e66825d053940b
M Isa Al Anshori <isabungsu@gmail.com>
2023-12-12 12:40:16 +0700

    setup Vercel deployment

--- app/models/index.js
+++ app/models/index.js
@@ -6,6 +6,7 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD, {
         host: process.env.DB_HOST,
         dialect: 'mysql',
+        dialectModule: require('mysql2'),
         operatorAlias: false,
         pool: {
             max: 5,
